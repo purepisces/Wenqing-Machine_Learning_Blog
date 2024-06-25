940918450d291f5f290a1ffdd00a531133ad3d72
purepisces <wenqingc@andrew.cmu.edu>
2024-06-25 10:56:12 -0400

    add exampleoutput and add original code

---
+++ Code-Implementation/Decision-Tree/original_code/decision_tree.py
@@ -0,0 +1,46 @@
+import argparse
+
+class Node:
+    '''
+    Here is an arbitrary Node class that will form the basis of your decision
+    tree. 
+    Note:
+        - the attributes provided are not exhaustive: you may add and remove
+        attributes as needed, and you may allow the Node to take in initial
+        arguments as well
+        - you may add any methods to the Node class if desired 
+    '''
+    def __init__(self):
+        self.left = None
+        self.right = None
+        self.attr = None
+        self.vote = None
+    
+def print_tree(Node):
+    pass
+
+if __name__ == '__main__':
+    # This takes care of command line argument parsing for you!
+    # To access a specific argument, simply access args.<argument name>.
+    # For example, to get the train_input path, you can use `args.train_input`.
+    parser = argparse.ArgumentParser()
+    parser.add_argument("train_input", type=str, help='path to training input .tsv file')
+    parser.add_argument("test_input", type=str, help='path to the test input .tsv file')
+    parser.add_argument("max_depth", type=int, 
+                        help='maximum depth to which the tree should be built')
+    parser.add_argument("train_out", type=str, 
+                        help='path to output .txt file to which the feature extractions on the training data should be written')
+    parser.add_argument("test_out", type=str, 
+                        help='path to output .txt file to which the feature extractions on the test data should be written')
+    parser.add_argument("metrics_out", type=str, 
+                        help='path of the output .txt file to which metrics such as train and test error should be written')
+    parser.add_argument("print_out", type=str,
+                        help='path of the output .txt file to which the printed tree should be written')
+    args = parser.parse_args()
+    
+    #Here's an example of how to use argparse
+    print_out = args.print_out
+
+    #Here is a recommended way to print the tree to a file
+    # with open(print_out, "w") as file:
+    #     print_tree(dTree, file)
